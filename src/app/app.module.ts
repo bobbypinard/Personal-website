d5055cb1f1092bd866041325d9259ecb3b2ca57c
Bobby <bobby.pinard@gmail.com>
2022-02-19 17:01:17 -0600

    Components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { FrameworksComponent } from './components/frameworks/frameworks.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    ContactComponent,
+    IntroComponent,
+    FrameworksComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
