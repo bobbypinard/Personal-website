0e111a44ac877813a871d26590523f142d51b01c
Bobby <bobby.pinard@gmail.com>
2022-02-19 19:55:37 -0600

    Projects MVP

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -6,6 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+  projects: any = [
+    {
+      title: "Personal Website",
+      link: "/",
+      description: "The website you're currently on",
+      mobilePic: "assets/Screenshots/Website - Mobile.png",
+      webPic: "assets/Screenshots/Website - Web.png"
+    },
+    {
+      title: "Into The Void",
+      link: "/",
+      description: "A mental health app for writing down thoughts and sending them to the void.",
+      mobilePic: "assets/Screenshots/Website - Mobile.png",
+      webPic: "assets/Screenshots/Website - Web.png"
+    }
+  ]
 
   constructor() { }
 
