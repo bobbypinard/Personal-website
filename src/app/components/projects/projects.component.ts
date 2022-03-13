a3f1a8248f1c980c3d47c5db9854b0782d9f81f9
Bobby <bobby.pinard@gmail.com>
2022-03-12 22:25:53 -0500

    Moved stuff around a bit

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -27,9 +27,26 @@ export class ProjectsComponent implements OnInit {
     }
   ]
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
-
 }
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
